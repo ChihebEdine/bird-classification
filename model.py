752f39d85da130eb0fc56a1b672f2bac66a8a0ca
Chiheb <chihebnajjar22@gmail.com>
2020-11-24 23:34:58 +0100

    add new model

--- model.py
+++ model.py
@@ -1,22 +1,25 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 
 nclasses = 20 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv3 = nn.Conv2d(20, 20, kernel_size=5)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, nclasses)
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.relu(F.max_pool2d(self.conv3(x), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        return self.fc2(x)
+        self.inception = models.inception_v3(pretrained=True, aux_logits=False)
+        for param in self.inception.parameters():
+           param.requires_grad = False
+
+        self.inception.fc = nn.Linear(2048, nclasses)
+
+        self.resnet = models.resnet50(pretrained=True)
+        for param in self.resnet.parameters():
+            param.requires_grad = False
+
+        self.resnet.fc = nn.Linear(2048, nclasses)
+
+    def forward(self, x): 
+        return self.resnet(x) + self.inception(x)
