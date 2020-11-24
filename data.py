5e9ed2ca2774c27f4c7089956e057a34b5b690b7
Chiheb <chihebnajjar22@gmail.com>
2020-11-24 23:37:08 +0100

    add data transforms

--- data.py
+++ data.py
@@ -7,11 +7,18 @@ import torchvision.transforms as transforms
 # by default, we resize the images to 64 x 64 in size
 # and normalize them to mean = 0 and standard-deviation = 1 based on statistics collected from
 # the training set
-data_transforms = transforms.Compose([
-    transforms.Resize((64, 64)),
+data_transforms_train = transforms.Compose([
+    transforms.Resize((300, 300)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.ColorJitter(),
+    transforms.RandomPerspective(),
+    transforms.RandomHorizontalFlip(),
 ])
 
 
+data_transforms_test = transforms.Compose([
+    transforms.Resize((300, 300)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
